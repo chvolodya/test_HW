36c2d9f663966f7055b56988abf232cbef35e6f3
chvolodya <chvolodya@gmail.com>
2025-11-12 16:13:10 +0500

    коммит в ветке тест

--- first_bot.py
+++ first_bot.py
@@ -6,5 +6,5 @@ bot = telebot.TeleBot(TG_token)
 def handle_start(message):
     bot.send_message(message.chat.id, "Hello Vika and Vovka")
     bot.send_message(message.chat.id, "Branch Test")
-
+    bot.send_message(message.chat.id, "Commit Test")
 bot.polling(none_stop=True)
