0ee073813a777d5c8a022220c05ecb650efa2d40
chvolodya <chvolodya@gmail.com>
2025-11-20 12:03:27 +0500

    Коммит для проверки развертывания через GitHub

--- first_bot.py
+++ first_bot.py
@@ -5,11 +5,6 @@ bot = telebot.TeleBot(TG_token)
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, "Hello Vika and Vovka")
-    bot.send_message(message.chat.id, "Branch Test")
-    bot.send_message(message.chat.id, "Commit Master")
-    bot.send_message(message.chat.id, "Commit Test")
-    bot.send_message(message.chat.id, "Change Master")
-    bot.send_message(message.chat.id, "Change test")
-    bot.send_message(message.chat.id, "Changed in Github")
-    bot.send_message(message.chat.id, "Changed in Home")
+    bot.send_message(message.chat.id, "Test Change from local and push over Github")
+    
 bot.polling(none_stop=True)
