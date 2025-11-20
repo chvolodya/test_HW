864778dfdffefd25e331976584fdf5f85873a9b5
chvolodya <chvolodya@gmail.com>
2025-11-20 16:57:27 +0500

    С удалением файлов и попыткой вторую функцию добавить

--- first_bot.py
+++ first_bot.py
@@ -8,4 +8,8 @@ def handle_start(message):
     bot.send_message(message.chat.id, "Test Change from local and push over Github")
     bot.send_message(message.chat.id, "Test Change from local and Repush over Github")
     
+@bot.message_handler(commands=['stop'])
+def handle_stop(message):
+    bot.send_message(message.chat.id, "GoodBye Vika and Vovka")
+    
 bot.polling(none_stop=True)
