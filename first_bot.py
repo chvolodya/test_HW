3ec8e3a07771441c641e1c08b4b22180911987e9
chvolodya <chvolodya@gmail.com>
2025-12-03 16:55:09 +0500

    Кнопки из списка - работающая + прокси

--- first_bot.py
+++ first_bot.py
@@ -8,18 +8,24 @@ apihelper.proxy = Proxy_config
 bot = telebot.TeleBot(TG_token)
 
 
-buttons_name = ['добавить', 'удалить' , 'пример']
+buttons_name = ['добавить', 'удалить' , 'пример', 'четвертая']
 
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    keyboard = types.ReplyKeyboardMarkup(buttons_name)
+    
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+    buttons = [types.KeyboardButton(i) for i in buttons_name]
+    keyboard.add(*buttons)
+    
+       
+    
     
     bot.reply_to(message, 'Привет! Я бот.', reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['stop'])
 def handle_stop(message):
-    bot.send_message(message.chat.id, "GoodBye Vika and Vika")
+    bot.send_message(message.chat.id, "GoodBye Vika and Vovka")
     
 bot.polling(none_stop=True)
